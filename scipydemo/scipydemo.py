371b613442860856a3bb7e839e0435fecaaa63ad
A. Belard <angebelard@Gmail.com>
2022-01-27 21:45:35 +0200

    Update scipydemo.py

--- scipydemo/scipydemo.py
+++ scipydemo/scipydemo.py
@@ -1,9 +1,5 @@
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
+import matplotlibdemo as mpl
+import matplotlibdemo.pyplot as plt
 from scipy import optimize
 
-x=2
-np.info(optimize)
-
-y=2
