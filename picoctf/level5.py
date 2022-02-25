6666b0ea1deacdf5261c40da83376e75628af4e0
A. Belard <angebelard@Gmail.com>
2022-02-25 07:08:47 +0200

    level 5

---
+++ picoctf/level5.py
@@ -0,0 +1,46 @@
+import hashlib
+
+### THIS FUNCTION WILL NOT HELP YOU FIND THE FLAG --LT ########################
+def str_xor(secret, key):
+    #extend key to secret length
+    new_key = key
+    i = 0
+    while len(new_key) < len(secret):
+        new_key = new_key + key[i]
+        i = (i + 1) % len(key)        
+    return "".join([chr(ord(secret_c) ^ ord(new_key_c)) for (secret_c,new_key_c) in zip(secret,new_key)])
+###############################################################################
+
+flag_enc = open('level5.flag.txt.enc', 'rb').read()
+correct_pw_hash = open('level5.hash.bin', 'rb').read()
+
+
+def hash_pw(pw_str):
+    pw_bytes = bytearray()
+    pw_bytes.extend(pw_str.encode())
+    m = hashlib.md5()
+    m.update(pw_bytes)
+    return m.digest()
+
+
+def level_5_pw_check(passwd):
+    user_pw = passwd
+    user_pw_hash = hash_pw(user_pw)
+    
+    if( user_pw_hash == correct_pw_hash ):
+        print("Welcome back... your flag, user:")
+        decryption = str_xor(flag_enc.decode(), user_pw)
+        print(decryption)
+        return decryption
+    return False
+
+count = 0
+with open("dictionary.txt", "r") as dictionary:
+    for line in dictionary:
+        print(line)
+        count += 1
+        check = level_5_pw_check(line.strip())
+        if check:
+            break
+
+print(count)
