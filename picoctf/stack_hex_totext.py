23ff50708a3a72d4404f990c60c75c624d050966
A. Belard <angebelard@Gmail.com>
2022-04-08 01:16:29 +0200

    Delete stack_hex_totext.py

--- picoctf/stack_hex_totext.py
+++
@@ -1,7 +0,0 @@
-myhex = "ocip{FTC0l_I4_t5m_ll0m_y_y3n58a025e3ÿ�}"
-mytext = ""
-for i in range(0, len(myhex), 4):
-    part = myhex[i:i+4]
-    mytext += "".join(reversed([str(k) for k in part]))
-
-print(mytext)
