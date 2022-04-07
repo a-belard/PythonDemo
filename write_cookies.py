8cef3217b0f89a2278e06f9235021816045721b8
A. Belard <angebelard@Gmail.com>
2022-04-08 01:17:06 +0200

    Create write_cookies.py

---
+++ write_cookies.py
@@ -0,0 +1,5 @@
+cookiesfile = open("cookies.txt", "w+")
+cookie_names = ["snickerdoodle", "chocolate chip", "oatmeal raisin", "gingersnap", "shortbread", "peanut butter", "whoopie pie", "sugar", "molasses", "kiss", "biscotti", "butter", "spritz", "snowball", "drop", "thumbprint", "pinwheel", "wafer", "macaroon", "fortune", "crinkle", "icebox", "gingerbread", "tassie", "lebkuchen", "macaron", "black and white", "white chocolate macadamia"]
+for i in cookie_names:
+    cookiesfile.write(i + "\n")
+cookiesfile.flush()
